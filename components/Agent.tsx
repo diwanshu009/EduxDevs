483aa6c689aa276483727a99623bddb0d6f38776
diwanshu009 <goddiwanshu@gmail.com>
2025-03-27 12:07:48 +0530

    ui page fixed along with setting google gemini

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-enum CallStatus{
+enum CallStatus {
     INACTIVE = 'INACTIVE',
     CONNECTING = 'CONNECTING',
     ACTIVE = 'ACTIVE',
@@ -15,7 +15,7 @@ export default function Agent({ userName }: AgentProps) {
         "What's your name?",
         "My name is abcd"
     ]
-    const lastMessage = messages[messages.length-1]
+    const lastMessage = messages[messages.length - 1]
 
     return (
         <>
@@ -36,7 +36,7 @@ export default function Agent({ userName }: AgentProps) {
                 </div>
             </div>
 
-            {messages.length>0 && (
+            {messages.length > 0 && (
                 <div className="transcript-border">
                     <div className="transcript">
                         <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0', 'animate-fadeIn opacity-100')}>
@@ -49,17 +49,17 @@ export default function Agent({ userName }: AgentProps) {
             <div className="w-full flex justify-center">
                 {callStatus !== 'ACTIVE' ? (
                     <button className="relative btn-call">
-                        <span className={cn('absolute animate-ping rounded-full opacity-75', callStatus !== 'CONNECTING' & 'hidden')}/>
+                        <span className={cn('absolute animate-ping rounded-full opacity-75', callStatus !== 'CONNECTING' && 'hidden')} />
 
                         <span>
-                        {callStatus === 'INACTIVE' || callStatus === 'FINISHED' ? 'Call' : '. . . '}
+                            {callStatus === 'INACTIVE' || callStatus === 'FINISHED' ? 'Call' : '. . .'}
                         </span>
                     </button>
                 ) : (
                     <button className="btn-disconnect">
                         End
                     </button>
-                ) }
+                )}
             </div>
         </>
     )
