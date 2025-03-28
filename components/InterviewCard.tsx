69465e52383332e1c8f7e8b588d9b154f39f94e5
diwanshu009 <goddiwanshu@gmail.com>
2025-03-28 18:37:40 +0530

    interview session page implemented

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,7 +5,7 @@ import { Button } from './ui/button'
 import Link from 'next/link'
 import DisplayTechIcons from './DisplayTechIcons'
 
-export default function InterviewCard({interviewId, userId, role, type, techstack, createdAt} : InterviewCardProps) {
+export default function InterviewCard({id, userId, role, type, techstack, createdAt} : InterviewCardProps) {
     const feedback = null as Feedback | null
 
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type
@@ -52,7 +52,7 @@ export default function InterviewCard({interviewId, userId, role, type, techstac
                 <div className='flex flex-row justify-between'>
                     <DisplayTechIcons techStack={techstack} />
                     <Button className='btn-primary'>
-                        <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>
+                        <Link href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}>
                             {feedback ? 'Check feedback' : 'View Interview'}
                         </Link>
                     </Button>
