0401dc605b40ddbd5893bb0491f7223e4571c7e4
diwanshu009 <goddiwanshu@gmail.com>
2025-03-28 17:54:30 +0530

    fixed interview generating page

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -7,19 +7,22 @@ import DisplayTechIcons from './DisplayTechIcons'
 
 export default function InterviewCard({interviewId, userId, role, type, techstack, createdAt} : InterviewCardProps) {
     const feedback = null as Feedback | null
+
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type
+    
     const badgeColor = {
-        Behavioral: "bg-light-400",
+        Behavioural: "bg-light-400",
         Mixed: "bg-light-600",
         Technical: "bg-light-800",
     }[normalizedType] || "bg-light-600";
+
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY')
 
     return (
         <div className='card-border w-[360px] max-sm:w-full min-h-96'>
             <div className='card-interview'>
                 <div>
-                    <div className={cn('absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600',badgeColor)}>
+                    <div className={cn('absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg',badgeColor)}>
                         <p className='badge-text'>{normalizedType}</p>
                     </div>
 
